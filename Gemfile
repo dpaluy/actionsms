16de88261f459fe4d4f37f15346c35c0f8fce422
David Paluy <david@paluy.org>
2014-12-11 16:37:15 +0200

    nexmo added

--- Gemfile
+++ Gemfile
@@ -1,12 +1,15 @@
 source "http://rubygems.org"
 
 gem "activesupport", ">= 3.2"
+gem 'twilio-ruby', '~> 3.12'
+gem 'nexmo'
 
 group :development do
   gem 'rspec', '~>2.14.1'
   gem 'rdoc'
   gem 'bundler'
   gem 'jeweler'
+  gem 'pry'
 end
 
 group :test do
---
+++ lib/sms/method/nexmo.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'nexmo'
+
+class Sms
+  module Method
+
+    class Nexmo
+      def initialize(account_sid, auth_token, default_from = nil)
+        @account_sid = account_sid
+        @auth_token = auth_token
+        @from = default_from
+      end
+
+      # @param [Sms::Message] sms
+      def deliver!(sms)
+        from = sms.from || @from
+        client.send_message(from: from, to: sms.to, text: sms.text)
+      end
+
+      private
+      # Set up a client to talk to the Nexmo REST API
+      def client
+        @client ||= ::Nexmo::Client.new(key: @account_sid, secret: @auth_token)
+      end
+    end
+
+  end
+end
